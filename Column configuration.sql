76ce459d6c22aa2f3161e05331ae858c2f7e7cde
machistore <machi.mcd@gmail.com>
2022-12-25 13:54:13 +0900

    edited .gitignore

---
+++ Column configuration.sql
@@ -0,0 +1,6 @@
+PRAGMA table_info('authors');
+PRAGMA table_info('item_types');
+PRAGMA table_info('items');
+PRAGMA table_info('publishers');
+PRAGMA table_info('terms');
+PRAGMA table_info('work_a');
